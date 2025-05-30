9c292f922e9bbe5dc0ab261971c791a41c32a21c
Saket Jha <saketjha0324@gmail.com>
2025-05-30 14:36:47 +0530

    Update schema.py

--- backend/fastapi/app/routes/schema.py
+++ backend/fastapi/app/routes/schema.py
@@ -1,6 +1,7 @@
 # Response schema
 from pydantic import BaseModel 
 from datetime import datetime
+from typing import Optional, List
 
 
 class FileInfo(BaseModel):
@@ -9,6 +10,45 @@ class FileInfo(BaseModel):
     upload_time: datetime
     mime_type: str
     size: int
+    folder_id: Optional[int] = None  
 
     class Config:
-        from_attributes = True  # Use in Pydantic v2
+        from_attributes = True  
+        
+
+class FolderInfo(BaseModel):
+    id: int
+    name: str
+    parent_id: Optional[int]
+
+    class Config:
+        from_attributes = True
+
+
+class FileInFolder(BaseModel):
+    id: str
+    filename: str
+    upload_time: datetime
+    mime_type: str
+    size: int
+
+    class Config:
+        from_attributes = True
+
+
+class SubFolderInfo(BaseModel):
+    id: int
+    name: str
+    parent_id: Optional[int]
+
+    class Config:
+        from_attributes = True
+
+
+class FolderDetails(BaseModel):
+    id: int
+    name: str
+    owner_id: int
+    parent_id: Optional[int]
+    files: List[FileInfo]
+    subfolders: List[SubFolderInfo]
